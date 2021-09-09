4219f39f6b06620adeb221aed9496e58d7f628a5
Michele Tobias <mmtobias@ucdavis.edu>
2021-09-09 08:35:21 -0700

    Add goals

---
+++ code/scrape_exposure_data.R
@@ -0,0 +1,9 @@
+#GOAL: 
+# 1. scrape the data from the UC Davis Potential Worksite Exposure Reporting (AB 685) report online
+# 2. clean the data 
+    # a. make the building names match the campus shapefile names
+    # b. make separate columns for start and end dates for potential exposure
+# 3. remove duplicates
+# 4. write a csv
+
+#URL: https://campusready.ucdavis.edu/potential-exposure
