97141182e052d7a889f95d5fa00356365e13ebf8
Michele Tobias <mmtobias@ucdavis.edu>
2021-09-09 09:06:59 -0700

    clarifying goals

--- code/scrape_exposure_data.R
+++ code/scrape_exposure_data.R
@@ -4,6 +4,9 @@
     # a. make the building names match the campus shapefile names
     # b. make separate columns for start and end dates for potential exposure
 # 3. remove duplicates
-# 4. write a csv
+# 4. write a csv with the exposure data
+# 5. join the exposure data to the campus building data & export a json file
 
-#URL: https://campusready.ucdavis.edu/potential-exposure
+#Worksite Exposure URL: https://campusready.ucdavis.edu/potential-exposure
+
+#Campus Building data URL: https://data-ucda.opendata.arcgis.com/datasets/ucdavis-campus-buildings/explore?location=38.534593%2C-121.792150%2C13.71 
